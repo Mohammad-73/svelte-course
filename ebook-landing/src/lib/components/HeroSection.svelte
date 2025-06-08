60dd4799cb41a93d5aa859035fef11d7b6c062fe
khalili <m.khalili7310@gmail.com>
2025-06-08 10:07:46 +0330

    Hero section styling

--- ebook-landing/src/lib/components/HeroSection.svelte
+++ ebook-landing/src/lib/components/HeroSection.svelte
@@ -6,7 +6,7 @@
 <section class="hero">
   <div class="hero-showcase text-center bold">
     <h1 class="purple">MOVE TO</h1>
-    <h1 class="purple mb-xs">IRAN</h1>
+    <h1 class="purple mb-xs">SPAIN</h1>
     <h4 class="italic semi-bold">
       There's no need to make the same mistakes I made.
     </h4>
@@ -25,3 +25,46 @@
     </p>
   </div>
 </section>
+
+<style>
+  .hero {
+    display: flex;
+    height: 100vh;
+    overflow-y: hidden;
+  }
+
+  .hero-showcase {
+    position: relative;
+    width: 50%;
+    padding: 100px 16px 0 16px;
+    background: linear-gradient(
+      135deg,
+      #e2cbff,
+      #e2cbff 25%,
+      #deeaff 50%,
+      #deeaff 75%,
+      #927bce
+    );
+  }
+
+  .hero-text {
+    padding: 100px 5%;
+    width: 50%;
+    background-color: black;
+  }
+
+  .book-cover {
+    position: absolute;
+    right: 2%;
+    bottom: 0;
+    width: 65%;
+    transform: translateY(30%);
+  }
+  .phone-cover {
+    position: absolute;
+    right: 52%;
+    bottom: 0;
+    width: 35%;
+    transform: translateY(25%);
+  }
+</style>
