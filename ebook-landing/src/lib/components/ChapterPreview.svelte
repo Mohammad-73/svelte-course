fda5daee1a1d804de4367d4b78ee68aab93665d1
Mohammad-73 <m.khalili7310@gmail.com>
2025-06-12 11:15:57 +0330

    Chapter preview Svelte Templating language usage of each keyword

--- ebook-landing/src/lib/components/ChapterPreview.svelte
+++ ebook-landing/src/lib/components/ChapterPreview.svelte
@@ -1,16 +1,52 @@
+<script>
+  const chapters = [
+    {
+      number: 1,
+      title: " Navigating the Spanish bureaucracy",
+      strapline: "Understanding Visas, Residency, and Legal Essentials",
+      excerpt:
+        "Moving to Spain involves navigating a labyrinth of bureaucratic processes that can feel overwhelming at first. This chapter breaks down the essential steps required to secure your visa, establish residency, and handle other legal formalities. Whether you're planning a short-term stay or seeking permanent residency, understanding Spain's legal framework is crucial. We'll explore the different types of Visas available, from student to work visas, and provide tips on how to streamline your application process. additionally, this chapter covers essential documentation, such as obtaining a Numero de Identidad de Extranjero (NIE), which is your key to unlocking many services in Spain.",
+    },
+    {
+      number: 2,
+      title: "Finding Your Dream Home in Spain",
+      strapline:
+        "From Coastal Villas to Urban Apartments: Where and How to Live",
+      excerpt:
+        "Spain offers a diverse array of living options, from picturesque coastal towns to bustling city centers. This chapter is your guide to finding...",
+    },
+    {
+      number: 3,
+      title: "Mastering the Spanish Job Market",
+      strapline: "Strategies for Employment and Freelancing in a New Culture",
+      excerpt:
+        "Finding a job in Spain can be a challenging but rewarding experience, especially if you're not familiar with the local job market...",
+    },
+    {
+      number: 4,
+      title: "Embracing the Spanish Lifestyle",
+      strapline: "Cultural Norms, Social Etiquette, and Making New Connections",
+      excerpt:
+        "Moving to Spain isn't just about changing your location; it's about immersing yourself in a new way of life. This chapter explore...",
+    },
+  ];
+</script>
+
 <section class="chapter-preview default-margin">
   <h2 class="mb-l">What you're getting</h2>
   <div class="chapter-container">
     <ul>
-      <li>
-        <button
-          class="chapter-title"
-          aria-controls="chapter-info-1"
-          aria-expanded="true"
-        >
-          <h3>Chapter 1: Navigating the Spanish bureaucracy</h3>
-        </button>
-      </li>
+      {#each chapters as chapter}
+        <li>
+          <button
+            class="chapter-title"
+            aria-controls={`chapter-info-${chapter.number}`}
+            aria-expanded="true"
+          >
+            <h3>Chapter {chapter.number}: {chapter.title}</h3>
+          </button>
+        </li>
+      {/each}
     </ul>
     <div class="chapter-info">
       <h3 class="chapter-strapline italic mb-s">
