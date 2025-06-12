f20ac2996368bfc4959f3e1364962fe37753e7e3
Mohammad-73 <m.khalili7310@gmail.com>
2025-06-12 11:31:32 +0330

    Chapter preview Derive state to simplify code structure

--- ebook-landing/src/lib/components/ChapterPreview.svelte
+++ ebook-landing/src/lib/components/ChapterPreview.svelte
@@ -29,9 +29,19 @@
       excerpt:
         "Moving to Spain isn't just about changing your location; it's about immersing yourself in a new way of life. This chapter explore...",
     },
+    {
+      number: 5,
+      title: "Budgeting and Finances in Spain",
+      strapline: "Cultural Norms, Social Etiquette, and Making New Connections",
+      excerpt:
+        "Successfully managing your finances is crucial to enjoying a stress-free life in Spain. This chapter provides practical guidance on how to budget for your new life, manage your expenses, and navigate Spain's Financial systems.",
+    },
   ];
 
   let selectedChapterNumber = $state(1);
+  let selectedChapter = $derived(
+    chapters.find((chapter) => chapter.number === selectedChapterNumber)
+  );
 
   function selectChapter(chapter) {
     selectedChapterNumber = chapter.number;
@@ -59,19 +69,10 @@
     </ul>
     <div class="chapter-info">
       <h3 class="chapter-strapline italic mb-s">
-        Understanding Visas, Residency, and Legal Essentials
+        {selectedChapter.title}
       </h3>
       <p>
-        Moving to Spain involves navigating a labyrinth of bureaucratic
-        processes that can feel overwhelming at first. This chapter breaks down
-        the essential steps required to secure your visa, establish residency,
-        and handle other legal formalities. Whether you're planning a short-term
-        stay or seeking permanent residency, understanding Spain's legal
-        framework is crucial. We'll explore the different types of Visas
-        available, from student to work visas, and provide tips on how to
-        streamline your application process. additionally, this chapter covers
-        essential documentation, such as obtaining a Numero de Identidad de
-        Extranjero (NIE), which is your key to unlocking many services in Spain.
+        {selectedChapter.excerpt}
       </p>
     </div>
   </div>
