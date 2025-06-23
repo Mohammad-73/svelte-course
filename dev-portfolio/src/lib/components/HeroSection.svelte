fd072ddaf1696a345c2f620a6e6f46756a60bf30
Mohammad-73 <m.khalili7310@gmail.com>
2025-06-23 14:06:03 +0330

    Hero section

---
+++ dev-portfolio/src/lib/components/HeroSection.svelte
@@ -0,0 +1,13 @@
+<section class="hero-section default-margin">
+  <h3>Hello! I'm Mohammad</h3>
+  <div class="underscore mb-m"></div>
+  <h1>I'm a Svelte developer crafting intuitive and performant</h1>
+  <h1 class="dark-grey">web experience.</h1>
+  <button>Let's talk!</button>
+</section>
+
+<style>
+  .hero-section {
+    padding-top: 60px;
+  }
+</style>
