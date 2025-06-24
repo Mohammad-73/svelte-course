cc03d47e0a06e923086029b0c71ceb907b41b2f7
Mohammad-73 <m.khalili7310@gmail.com>
2025-06-24 09:22:53 +0330

    Flexible Button component

--- dev-portfolio/src/lib/components/index.ts
+++ dev-portfolio/src/lib/components/index.ts
@@ -1,2 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
-export { default as HeroSection } from "./HeroSection.svelte";
+export { default as HeroSection } from "./sections/HeroSection.svelte";
+export { default as Button } from "./Button.svelte";
--- dev-portfolio/src/lib/components/sections/HeroSection.svelte
+++ dev-portfolio/src/lib/components/sections/HeroSection.svelte
@@ -1,9 +1,18 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { Button } from "$components";
+
+  function onclick() {
+    goto("/#contact-form");
+  }
+</script>
+
 <section class="hero-section default-margin">
   <h3>Hello! I'm Mohammad</h3>
   <div class="underscore mb-m"></div>
   <h1>I'm a Svelte developer crafting intuitive and performant</h1>
   <h1 class="dark-grey">web experience.</h1>
-  <button>Let's talk!</button>
+  <Button {onclick}>Let's talk!</Button>
 </section>
 
 <style>
