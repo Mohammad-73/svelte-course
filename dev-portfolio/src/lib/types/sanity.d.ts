c48d44f02a2a1e3c4d6040a79af835a7556edbd6
khalili <m.khalili7310@gmail.com>
2025-06-28 15:19:54 +0330

    Fetch data from Sanity in SveltKit frontend

---
+++ dev-portfolio/src/lib/types/sanity.d.ts
@@ -0,0 +1,12 @@
+export type SanityImagePaletteSwatch = {
+  _type: "sanity.imagePaletteSwatch";
+  background?: string;
+  foreground?: string;
+  population?: number;
+  title?: string;
+};
+
+export type SanityImagePalette = {
+  _type: "sanity.imagePalette";
+  darkMuted?: SanityImagePalette;
+};
