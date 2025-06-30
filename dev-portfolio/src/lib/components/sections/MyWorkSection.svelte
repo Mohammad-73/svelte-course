ab7929d12425b0ec9df59ab2a749ab28baab629b
khalili <m.khalili7310@gmail.com>
2025-06-30 11:15:19 +0330

    My work section styling

--- dev-portfolio/src/lib/components/sections/MyWorkSection.svelte
+++ dev-portfolio/src/lib/components/sections/MyWorkSection.svelte
@@ -42,3 +42,49 @@
     </div>
   </div>
 </section>
+
+<style>
+  img {
+    width: 100%;
+    border-radius: 20px;
+    object-fit: cover;
+    cursor: pointer;
+  }
+
+  .more-project-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .more-projects_container img {
+    height: 350px;
+  }
+
+  .main-project img {
+    max-height: 450px;
+  }
+
+  .project {
+    width: 48%;
+  }
+
+  .project-info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .btn-to-article {
+    display: block;
+    font-size: 40px;
+    border: 1px solid black;
+    border-radius: 40px;
+    padding: 0px 24px;
+  }
+
+  .company {
+    font-size: 18px;
+    margin-bottom: 0;
+  }
+</style>
