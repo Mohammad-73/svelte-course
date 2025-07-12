dab02eff27ab992577db3729ea1fc5039e8863f2
khalili <m.khalili7310@gmail.com>
2025-07-12 09:44:28 +0330

    Flexible Button Component

---
+++ book-nest/src/lib/components/Button.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  interface BasicProps {
+    children: Snippet;
+    isSecondary?: boolean;
+    isDanger?: boolean;
+    isMenu?: boolean;
+  }
+
+  interface ButtonProps extends BasicProps {
+    onclick: (e: MouseEvent) => void;
+    href?: never;
+  }
+
+  interface LinkProps extends BasicProps {
+    href: string;
+    onclick?: never;
+  }
+
+  type ComponentProps = ButtonProps | LinkProps;
+
+  let {
+    children,
+    href,
+    onclick,
+    isSecondary,
+    isDanger,
+    isMenu,
+    ...props
+  }: ButtonProps = $props();
+</script>
+
+{#if href}
+  <a
+    {href}
+    class="btn"
+    class:btn-secondary={isSecondary}
+    class:btn-danger={isDanger}
+    class:btn-menu={isMenu}
+  >
+    {@render children()}
+  </a>
+{:else}
+  <button
+    {...props}
+    class="btn"
+    class:btn-secondary={isSecondary}
+    class:btn-danger={isDanger}
+    class:btn-menu={isMenu}
+  >
+    {@render children()}
+  </button>
+{/if}
