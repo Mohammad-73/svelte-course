a5416064ba8715abd302d980f07e499b2c378d85
khalili <m.khalili7310@gmail.com>
2025-07-15 10:01:36 +0330

    Auth form styling

--- book-nest/src/lib/components/AuthForm.svelte
+++ book-nest/src/lib/components/AuthForm.svelte
@@ -11,7 +11,7 @@
 <div class="default-margin auth-container">
   <h1 class="mb-l">{isRegistration ? "Register" : "Login"}</h1>
   <div class="form-and-social-login">
-    <form>
+    <form class="auth-form">
       {#if isRegistration}
         <input placeholder="Name" type="text" name="name" />
       {/if}
@@ -40,3 +40,55 @@
     </div>
   </div>
 </div>
+
+<style>
+  .auth-container {
+    margin-top: 80px;
+  }
+
+  .form-and-social-login {
+    display: flex;
+  }
+
+  .auth-form {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    border-right: 1px solid grey;
+    padding-right: 80px;
+    width: 40%;
+  }
+
+  .auth-hint {
+    font-size: 16px;
+    color: grey;
+  }
+
+  .auth-error {
+    background-color: rgb(122, 35, 35);
+    color: white;
+    font-size: 18px;
+    border-radius: 12px;
+    padding: 12px;
+    width: 100%;
+    margin-bottom: 8px;
+  }
+
+  .auth-error:last-of-type {
+    margin-bottom: 16px;
+  }
+
+  .auth-form input {
+    width: 100%;
+    margin-bottom: 12px;
+  }
+
+  .auth-form input:last-of-type {
+    margin-bottom: 30px;
+  }
+
+  .social-login {
+    padding-left: 80px;
+    width: 40%;
+  }
+</style>
