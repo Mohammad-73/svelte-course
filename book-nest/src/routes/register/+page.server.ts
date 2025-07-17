c67a624eb5dc0202e515a2d8dcb2be9a6841b7c0
Mohammad-73 <m.khalili7310@gmail.com>
2025-07-17 10:36:50 +0330

    Supabase Signup with Email and password

--- book-nest/src/routes/register/+page.server.ts
+++ book-nest/src/routes/register/+page.server.ts
@@ -1,4 +1,10 @@
 import type { Actions } from "./$types";
+import { createClient } from "@supabase/supabase-js";
+import {
+  PUBLIC_SUPABASE_ANON_KEY,
+  PUBLIC_SUPABASE_URL,
+} from "$env/static/public";
+import { fail, redirect } from "@sveltejs/kit";
 
 interface ReturnObject {
   success: boolean;
@@ -43,7 +49,21 @@ export const actions = {
     }
 
     // Registration flow.
+    const supabase = createClient(
+      PUBLIC_SUPABASE_URL,
+      PUBLIC_SUPABASE_ANON_KEY
+    );
 
-    return returnObject;
+    const { data, error } = supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error || !data.user) {
+      returnObject.success = true;
+      return fail(400, returnObject as any);
+    }
+
+    redirect(303, "private/dashboard");
   },
 };
