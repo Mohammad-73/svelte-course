2ce73b124e90e528374123165dd5e00e4c88c455
khalili <m.khalili7310@gmail.com>
2025-07-21 09:47:02 +0330

    Set up context to keep track of user state

--- book-nest/src/lib/components/layout/Header.svelte
+++ book-nest/src/lib/components/layout/Header.svelte
@@ -1,6 +1,10 @@
 <script>
   import bookNestLogo from "$assets/app-logo.svg";
   import { Button } from "$components";
+  import { getUserState } from "$components/state/user-state.svelte";
+
+  let userContext = getUserState();
+  let { user } = $derived(userContext);
 </script>
 
 <header>
@@ -8,14 +12,24 @@
     <img class="logo" src={bookNestLogo} alt="Go to home" />
   </a>
   <nav>
-    <ul>
-      <li>
-        <Button isMenu={true} href="/register">Create account</Button>
-      </li>
-      <li>
-        <Button isMenu={true} isSecondary={true} href="/login">Login</Button>
-      </li>
-    </ul>
+    {#if !user}
+      <ul>
+        <li>
+          <Button isMenu={true} href="/register">Create account</Button>
+        </li>
+        <li>
+          <Button isMenu={true} isSecondary={true} href="/login">Login</Button>
+        </li>
+      </ul>
+    {:else}
+      <ul>
+        <li>
+          <Button isMenu={true} onclick={() => userContext.logout()}
+            >Logout</Button
+          >
+        </li>
+      </ul>
+    {/if}
   </nav>
 </header>
 
@@ -29,6 +43,7 @@
 
   ul {
     display: flex;
+    align-items: center;
     column-gap: 24px;
   }
 
