32f321ba77ddcb714f86d8330d6964efdbde244a
khalili <m.khalili7310@gmail.com>
2025-08-18 09:44:56 +0330

    Open AI integration and Send picture to chat gpt to get book array

--- book-nest/src/routes/api/scan-shelf/+server.ts
+++ book-nest/src/routes/api/scan-shelf/+server.ts
@@ -1,7 +1,45 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
+import OpenAI from "openai";
+import { OPENAI_API_KEY } from "$env/static/private";
+
+const openai = new OpenAI({
+  apiKey: OPENAI_API_KEY,
+});
 
 export const POST: RequestHandler = async ({ request }) => {
   const { base64 } = await request.json();
 
-  return json({ success: true });
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: `In the given image there will be...:
+            {
+              "bookTitle": "Harry Potter and the Deathly Hallows",
+              "author": "J.K. Rowling"
+              }
+              Please also make sure that you return an array, even if there is only one book visible on the image.`,
+          },
+          {
+            type: "image_url",
+            image_url: {
+              url: `data:image/jpeg;base64,${base64}`,
+              detail: "low",
+            },
+          },
+        ],
+      },
+    ],
+  });
+
+  const bookArrayString = response.choices[0].message.content
+    ?.replace(/```json|```/g, "")
+    .trim();
+  const bookArray = JSON.parse(bookArrayString || "");
+
+  return json({ bookArray });
 };
--- book-nest/src/routes/private/scan-shelf/+page.svelte
+++ book-nest/src/routes/private/scan-shelf/+page.svelte
@@ -5,6 +5,11 @@
 
   let isLoading = $state(false);
 
+  interface OpenAiBook {
+    author: string;
+    bookTitle: string;
+  }
+
   async function handleDrop(e: CustomEvent<any>) {
     const { acceptedFiles } = e.detail;
 
@@ -21,6 +26,9 @@
           },
           body: JSON.stringify({ base64: "hi" }),
         });
+
+        const result = (await response.json()) as { bookArray: OpenAiBook[] };
+        console.log(result);
       } catch (error) {
         console.log(error);
       }
